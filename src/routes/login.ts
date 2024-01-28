84ea60cd0154ec810cdbcfdc4c71d293791be9e8
origiladi <120328585+origiladi@users.noreply.github.com>
2024-01-28 16:33:17 +0200

    created a password change function

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,11 +1,12 @@
 import { Router} from "express";
-import { loginCheck} from '../controllers/user.controller'
+import { loginCheck, passwordChange} from '../controllers/user.controller'
 const loginRouter: Router = Router();
 
 import { celebrate, Joi } from 'celebrate';
 import { loginCheckSchema} from "../middlewares/celebrate/user.scema";
 
 loginRouter.post('/', celebrate(loginCheckSchema), loginCheck)
+loginRouter.patch('/', celebrate(loginCheckSchema), passwordChange)
 
 
 
