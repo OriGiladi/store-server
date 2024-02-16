f9007f075f482f4dc62bda22c715b6838d1ace8c
origiladi <120328585+origiladi@users.noreply.github.com>
2024-02-16 19:16:21 +0200

    done nothing special

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,4 @@
 import {User, UserModel} from '../models/user.model'
-import { TokenModel } from '../models/token.model';
 import { AdminModel } from '../models/admin.model';
 import { Request, Response, NextFunction} from "express";
 import bcrypt from 'bcrypt'
@@ -7,15 +6,6 @@ import jwt from 'jsonwebtoken'
 import { DEVELOPMENT_TOKEN_SECRET_KEY,NODE_ENV, SALT } from '../utils/constants';
 import { Unauthorize } from '../errors/unauthorize';
 import { NotFoundError } from '../errors/not-found-error';
-
-// function generateTokens(user){
-//     const mySecret = temp_TOKEN_SECRET_KEY as string 
-//     const myRefresh_Secret = temp_REFRESH_TOKEN_SECRET_KEY as string 
-//     const myToken: string | undefined = jwt.sign({id: user.id}, mySecret, { expiresIn: '10m' })
-//     const myRefreshToken: string | undefined = jwt.sign({id: user.id}, myRefresh_Secret)
-//     TokenModel.create({myRefreshToken})
-//     return { accessToken: myToken, refreshToken: myRefreshToken }
-// } 
 
 export const createUser = (req: Request, res: Response, next: NextFunction) => {
     const {firstName, lastName, email, password, image} = req.body
@@ -27,9 +17,8 @@ export const createUser = (req: Request, res: Response, next: NextFunction) => {
     .then(hash => {
         UserModel.create({firstName, lastName, email, password: hash, image})
         .then((user) => {
-            // const tokens = generateTokens(user)
             const mySecret = DEVELOPMENT_TOKEN_SECRET_KEY as string
-            const myToken: string | undefined = jwt.sign({id: user.id}, mySecret)
+            const myToken: string | undefined = jwt.sign({id: user.id}, mySecret, { expiresIn: '10m' })
             return res.send({message: 'Registering Succesful!',token: myToken});
         })
         .catch((error) => {
@@ -77,16 +66,14 @@ export const loginCheck = async (req: Request, res: Response, next: NextFunction
         const isPasswordValid = await bcrypt.compare(password, user.password);
 
         if (isPasswordValid) {
-            // const tokens = generateTokens(user)
-            //const mySecret = temp_TOKEN_SECRET_KEY as string 
             const mySecret = DEVELOPMENT_TOKEN_SECRET_KEY as string 
             const admin = await AdminModel.find({userId: user.id})
             let userJwt: string | undefined = undefined
             if(admin[0] === undefined){
-                userJwt = jwt.sign({id: user.id, userRole: "USER"}, mySecret) // TODO: get the roles from an enum in utils
+                userJwt = jwt.sign({id: user.id, userRole: "USER"}, mySecret, { expiresIn: '10m' }) // TODO: get the roles and expiresIn from an enum in utils
                 return res.send({message: 'Logging Succesful!',userJwt: userJwt});
             }
-            userJwt = jwt.sign({id: user.id, userRole: "ADMIN"}, mySecret) // TODO: get the roles from an enum in utils
+            userJwt = jwt.sign({id: user.id, userRole: "ADMIN"}, mySecret, { expiresIn: '10m' }) // TODO: get the roles and expiresIn from an enum in utils
             return res.send({message: 'Logging Succesful for admin!',userJwt: userJwt});
         } else {
             throw new Unauthorize("You are not authorize to log in!")
--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -2,7 +2,6 @@ import jwt, { Secret } from 'jsonwebtoken'
 import { DEVELOPMENT_TOKEN_SECRET_KEY, PRODUCTION_TOKEN_SECRET_KEY, NODE_ENV } from '../utils/constants';
 import { Request, Response, NextFunction } from "express";
 import { Unauthorize } from '../errors/unauthorize';
-import { TokenModel } from '../models/token.model';
 
 export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
 
@@ -10,22 +9,19 @@ export const authMiddleware = async (req: Request, res: Response, next: NextFunc
     if (!authorization || !authorization.startsWith('Bearer ')) 
       throw new Unauthorize('Authorization required')
     
-  const token = authorization.replace('Bearer ', ''); 
-    
+  const userJwt = authorization.replace('Bearer ', ''); 
     let payload: string | jwt.JwtPayload;
-
     try {
 
             payload = jwt.verify(
-            token,
+            userJwt,
             NODE_ENV === 'dev' ? (DEVELOPMENT_TOKEN_SECRET_KEY as Secret) : 
             NODE_ENV === 'production' ? (DEVELOPMENT_TOKEN_SECRET_KEY as Secret) :
             "foreign environment"
             );
+            req.user = payload;
         } catch (error) {
-          throw new Unauthorize('Authorization required!')
+            req.user = {error: "Authorization required!"}
         }
-
-        req.user = payload;
         next();
 }; 
