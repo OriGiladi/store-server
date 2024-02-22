c3c4f343638c95cb2899dffb5b9730a60235ae85
origiladi <120328585+origiladi@users.noreply.github.com>
2024-02-22 19:12:51 +0200

    added it

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,14 +1,14 @@
 import { Router} from "express";
 
 import { celebrate, Joi } from 'celebrate';
-import { createProductSchema, getProductSchema, updateProductSchema } from "../middlewares/celebrate/product.schema";
+import { createProductSchema, getProductSchema, rateProductSchema } from "../middlewares/celebrate/product.schema";
 import { getAllProducts, getSingleProduct, rateProduct } from "../controllers/product.controller";
 
 const productRouter: Router = Router();
 
 productRouter.get('/',celebrate(getProductSchema),  getAllProducts)
 productRouter.get('/:id',celebrate(getProductSchema),  getSingleProduct)
-productRouter.patch('/:id',celebrate(updateProductSchema),  rateProduct)
+productRouter.patch('/:id',celebrate(rateProductSchema),  rateProduct)
 
 
 export default productRouter; 
